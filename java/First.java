d0e6ce2f50ed8bd7705b13969fd730d544d73a02
aquajerry <huopopo_zeyang@163.com>
2018-07-29 19:55:03 +0800

    feat: java/First.java: command line parameter

--- java/First.java
+++ java/First.java
@@ -1,5 +1,7 @@
 public class First {
   public static void main(String[] args) {
     System.out.println("My first Java program.");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
   }
 }
