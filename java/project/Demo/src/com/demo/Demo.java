6100e65cc40ea36cd569db70c095f77a362b6c82
aquajerry <huopopo_zeyang@163.com>
2018-07-30 15:14:07 +0800

    update: com.demo.Demo: Welcome to Java lesson 1.8

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -1,33 +1,10 @@
 package com.demo;
 
 public class Demo {
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    byte b = 65;
-    short s = b;
-    //char c = b;
-    char c = '中';
-    s = (short)c;
-    c = (char)s;
-    int i = c;
-    i = s;
-    long l = i;
-    float f = l;
-    double d = f;
-    f = (float)d;
-    int num = 65;
-    char ch = (char)num;
 
-    double d2 = 5.123_456_789;
-    System.out.println("d2=" + d2);
-    float fl = (float)d2;
-    System.out.println("fl=" + fl);
-    int i2 = (int)d2;
-    System.out.println("i2=" + i2);
-    boolean bl2 = true;
-    //i2 = (int)bl2;
-    String str = "hello";
-    //i2 = (int)str;
   }
 
 }
