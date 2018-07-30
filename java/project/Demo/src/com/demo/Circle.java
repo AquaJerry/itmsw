f4a4d6c1eba5c56d1b0e647e560f1abc7d125c78
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:14:07 +0800

    update: com.demo.Circle: add String print(String)

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -2,10 +2,11 @@ package com.demo;
 
 public class Circle extends Shape {
 
-  /*@Override
-  public void print(String str) {
+  @Override
+  public String print(String str) {
     System.out.println(str + " in circle");
-  }*/
+    return "hello";
+  }
 
   public static void main(String[] args) {
     Circle c = new Circle();
