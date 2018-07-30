ff8439a14cb9037278fae2872b22d9c0357d971c
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:18:56 +0800

    update: com.demo.Demo: add some variable

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,9 +3,10 @@ package com.demo;
 public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int $_age = 30;
+    int Age5 = 30;
+    int Age5 = 20;
     float price = 5.6f;
-    System.out.println("age=" + $_age);
+    System.out.println("age=" + Age5);
     System.out.println("price=" + price);
   }
 
