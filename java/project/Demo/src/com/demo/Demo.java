691948b41be1f709f67929522f658d2008cd06f1
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:13:28 +0800

    fix: com.demo.Demo: age cannot be resolved to a variable

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,7 +3,7 @@ package com.demo;
 public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    //int age = 30;
+    int age;
     float price = 5.6f;
     System.out.println("age=" + age);
     System.out.println("price=" + price);
