28d32d05b5c90e1dfd1f5b6aeb9beefc5e5d0a2f
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:10:42 +0800

    feat: com.demo.Demo: using float

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     int age = 30;
+    float price = 5.6f;
     System.out.println("age=" + age);
+    System.out.println("price=" + price);
   }
 
 }
