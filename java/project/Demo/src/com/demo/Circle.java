7651b1fa3c39fe957abc960c7b32b78821120609
aquajerry <huopopo_zeyang@163.com>
2018-07-30 17:52:32 +0800

    update: com.demo.{Shape,Circle}: Welcome to Java lesson 1.12

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -1,18 +1,8 @@
 package com.demo;
 
 public class Circle extends Shape {
-  public Circle() {
-    System.out.println("Child class");
-  }
-
-  public void test() {
-    System.out.println("test in circle");
-  }
 
   public static void main(String[] args) {
-    Circle c = new Circle();
-    c.print();
-    c.test();
-    System.out.println(c.getClass());
+
   }
 }
--- java/project/Demo/src/com/demo/Shape.java
+++ java/project/Demo/src/com/demo/Shape.java
@@ -1,17 +1,5 @@
 package com.demo;
 
 public class Shape {
-  public Shape() {
-    System.out.println("Parent class");
-  }
 
-  public void print() {
-    System.out.println("Shape");
-  }
-
-  public static void main(String[] args) {
-    Shape s = new Shape();
-    s.print();
-    System.out.println(s.getClass());
-  }
 }
