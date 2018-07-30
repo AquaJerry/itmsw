9c83a0feaa316406cafced179e40378bf2b1b9a9
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:15:26 +0800

    fix: com.demo.Demo: The local variable age may not have been initialized

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,9 +3,9 @@ package com.demo;
 public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int age;
+    int _age = 30;
     float price = 5.6f;
-    System.out.println("age=" + age);
+    System.out.println("age=" + _age);
     System.out.println("price=" + price);
   }
 
