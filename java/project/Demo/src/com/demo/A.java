a1660007be118e241caa153c2d3c31e1d8acbaf3
aquajerry <huopopo_zeyang@163.com>
2018-07-30 17:37:49 +0800

    update: com.demo.A: call parent same-name method

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -15,10 +15,12 @@ public class A {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    A a = new A();
+    /*A a = new A();
     a.sum(5, 6);
     a.sum(5.6f, 6.1f);
-    a.sum(3, 4, 5);
+    a.sum(3, 4, 5);*/
+    B b = new B();
+    b.sum(10, 20);
   }
 
 }
