6e2b1d86c4d7c3e75a5c283d7fd39d05dee5d5af
aquajerry <huopopo_zeyang@163.com>
2018-07-30 11:08:28 +0800

    update: java/project: using eclipse

---
+++ java/project/Demo/src/com/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Demo {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Demo");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+  }
+
+}
