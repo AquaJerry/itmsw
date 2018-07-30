186fd6d1dc63a7e83dae9daa0d05ab04392bd08e
aquajerry <huopopo_zeyang@163.com>
2018-07-30 15:52:34 +0800

    update: com.demo.Student: get member

--- java/project/Demo/src/com/demo/Student.java
+++ java/project/Demo/src/com/demo/Student.java
@@ -10,12 +10,14 @@ public class Student {
 
   public Student(String userName) {
     System.out.println("userName=" + userName);
+    name = userName;
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     String str = new String("hello");
     Student s1 = new Student("tom");
+    System.out.println("s1.name=" + s1.name);
   }
 
 }
