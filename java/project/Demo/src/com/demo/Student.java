5977f8531ef102ff766968ef46b97ebafe17d37a
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:47:51 +0800

    update: com.demo.Student: to java lesson 1.13

--- java/project/Demo/src/com/demo/Student.java
+++ java/project/Demo/src/com/demo/Student.java
@@ -1,30 +1,11 @@
 package com.demo;
 
 public class Student {
-  String name;
-  int age;
-
-  public Student() {
-    System.out.println("Student instance created.");
-  }
-
-  public Student(String userName) {
-    System.out.println("userName=" + userName);
-    name = userName;
-  }
-
-  public void printName() {
+  public void printName(String name) {
     System.out.println("name=" + name);
   }
-
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    String str = new String("hello");
-    Student s1 = new Student("tom");
-    System.out.println("s1.name=" + s1.name);
-    s1.printName();
-    Student s2 = new Student("andy");
-    s2.printName();
+
   }
 
 }
