e4f6795d5cc08230317c0d551b64c25189d09dbc
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:38:55 +0800

    update: com.demo.Demo: Welcome to Java lesson 1.6

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -4,15 +4,8 @@ public class Demo {
   static int num;
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int Age5 = 0;
-    int age5 = 20;
-    {
-      float price = 5.6f;
-      System.out.println("price=" + price);
-    }
-    System.out.println("Age5=" + Age5);
-    System.out.println("age5=" + age5);
-    System.out.println("num=" + num);
+
+
   }
 
 }
