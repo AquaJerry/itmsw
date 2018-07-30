bf6035538e0ace4023d76481795f470c07d8e600
aquajerry <huopopo_zeyang@163.com>
2018-07-30 13:10:39 +0800

    update: com.demo.Demo: auto convert data type

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,7 +3,9 @@ package com.demo;
 public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    byte b = 65;
+    short s = b;
+    char c = b;
   }
 
 }
