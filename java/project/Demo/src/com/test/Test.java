79af21b84f4b78ba335c56f29f3f6d98b8b15f80
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:52:23 +0800

    feat: com.test.Test: using import

--- java/project/Demo/src/com/test/Test.java
+++ java/project/Demo/src/com/test/Test.java
@@ -1,10 +1,13 @@
 package com.test;
 
+import com.demo.Student;
+
 public class Test {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    Student s = new Student();
+    s.printName("tom");
   }
 
 }
