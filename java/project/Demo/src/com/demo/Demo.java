681037a898dffa44fb60ad148adc0ab4bbadc918
aquajerry <huopopo_zeyang@163.com>
2018-07-30 13:08:44 +0800

    update: com.demo.Demo: Welcome to Java lesson 1.7

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -1,29 +1,9 @@
 package com.demo;
 
 public class Demo {
-  static int num;
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    byte b = 'a';
-    System.out.println("b=" + b);
-    char c = '\u4e2d';
-    System.out.println("c=" + (int)c);
-    short s = 30000;
-    System.out.println("s=" + s);
-    int i = 100000000;
-    System.out.println("i=" + i);
-    long l = 1000000000000L;
-    System.out.println("l=" + l);
-    float f = 5.6f;
-    System.out.println("f=" + f);
-    double d = 5.6;
-    System.out.println("d=" + d);
-    boolean bl = true;
-    System.out.println("bl=" + bl);
-    String str = "hello";
-    String str2 = new String("hello");
-    System.out.println("str=" + str);
-    System.out.println("str2=" + str2);
+
   }
 
 }
