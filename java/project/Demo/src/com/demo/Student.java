7764909ddd8d82e42b6b8402578173f1b5cdacec
aquajerry <huopopo_zeyang@163.com>
2018-07-30 15:45:58 +0800

    feat: com.demo.Student: initial commit

---
+++ java/project/Demo/src/com/demo/Student.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Student {
+  String name;
+  int age;
+
+  public Student() {
+    System.out.println("Student instance created.");
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    String str = new String("hello");
+    Student s1 = new Student();
+  }
+
+}
