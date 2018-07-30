140a9d605a1438f771698c230e4c06921fcc34a7
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:29:58 +0800

    update: com.demo.Circle: add Shape s

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -9,7 +9,9 @@ public class Circle extends Shape {
   }
 
   public static void main(String[] args) {
-    Circle c = new Circle();
-    c.print1(100);
+    //Circle c = new Circle();
+    //c.print1(100);
+    Shape s = new Shape();
+    s.print1(200);
   }
 }
