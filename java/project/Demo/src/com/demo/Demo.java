afc4fd5fd6b9749ad3a140a138f95b8eb2c80dc2
aquajerry <huopopo_zeyang@163.com>
2018-07-30 15:16:53 +0800

    feat: com.demo.Demo: using method

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -1,10 +1,15 @@
 package com.demo;
 
 public class Demo {
+  int sum(int a, int b) {
+    System.out.println("a=" + a);
+    System.out.println("b=" + b);
+    return a + b;
+  }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    sum(5, 6);
   }
 
 }
