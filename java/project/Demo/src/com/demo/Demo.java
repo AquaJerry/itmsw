862c69412bb6205e7673c9ec1ddd4df4b526f7db
aquajerry <huopopo_zeyang@163.com>
2018-07-30 15:24:07 +0800

    update: com.demo.Demo: sum return void

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -1,16 +1,17 @@
 package com.demo;
 
 public class Demo {
-  static int sum(int a, int b) {
+  static void sum(int a, int b) {
     System.out.println("a=" + a);
     System.out.println("b=" + b);
-    return a + b;
+    //return a + b;
+    System.out.println(a + "+" + b + "=" + (a+b));
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int s = sum(5, 6);
-    System.out.println("s=" + s);
+    sum(5, 6);
+    //System.out.println("s=" + s);
   }
 
 }
