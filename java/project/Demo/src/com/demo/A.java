112323e2f13438c816560ce0981fbb611ad05595
aquajerry <huopopo_zeyang@163.com>
2018-07-30 17:04:40 +0800

    update: com.demo.A: multiple sum()

---
+++ java/project/Demo/src/com/demo/A.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class A {
+  public void sum(int a, int b) {
+
+  }
+
+  public void sum(int a, int b) {
+
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
