d2e74e8f4de1afbaecc20b84a4b17524eb104a80
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:28:05 +0800

    update: com.demo.Circle: override print of Shape

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -2,13 +2,14 @@ package com.demo;
 
 public class Circle extends Shape {
 
-  public String print(int str) {
+  @override
+  public String print1(int str) {
     System.out.println(str + " in circle");
     return "hello";
   }
 
   public static void main(String[] args) {
     Circle c = new Circle();
-    c.print(100);
+    c.print1(100);
   }
 }
