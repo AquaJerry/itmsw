0da669e92c9b278433b9d80a5d86da0a0db36786
aquajerry <huopopo_zeyang@163.com>
2018-07-30 17:10:33 +0800

    update: com.demo.A: sum()s

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -2,25 +2,21 @@ package com.demo;
 
 public class A {
   public void sum(int a, int b) {
-
-  }
-
-  public int sum(int a, int b) {
-
-    return 0;
+    System.out.println("int a+b=" + (a+b));
   }
 
   public void sum(int a, int b, int c) {
-
+    System.out.println("int a+b+c=" + (a+b+c));
   }
 
   public void sum(float a, float b) {
-
+    System.out.println("float a+b=" + (a+b));
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    A a = new A();
+    a.sum(5, 6);
   }
 
 }
