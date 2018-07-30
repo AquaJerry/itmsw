7bc67de66fe4d897439cd7f5a1db5c1bea4e10b8
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:40:07 +0800

    feat: com.demo.Demo: using byte

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
   static int num;
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    byte b = 65;
+    System.out.println("b=" + b);
 
   }
 
