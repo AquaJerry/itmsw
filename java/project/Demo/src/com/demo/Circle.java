1bb9aca981fd38e7a3f2aba1dd0ca5a799676be4
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:01:01 +0800

    feat: com.demo.{Shape,Circle}: using override

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -2,7 +2,13 @@ package com.demo;
 
 public class Circle extends Shape {
 
-  public static void main(String[] args) {
+  @Override
+  public void print(String str) {
+    System.out.println(str + " in circle");
+  }
 
+  public static void main(String[] args) {
+    Circle c = new Circle();
+    c.print("tom");
   }
 }
--- java/project/Demo/src/com/demo/Shape.java
+++ java/project/Demo/src/com/demo/Shape.java
@@ -1,5 +1,11 @@
 package com.demo;
 
 public class Shape {
+  public void print(String str) {
+    System.out.println(str + " in shape");
+  }
 
+  public void test() {
+    System.out.println("test in shape");
+  }
 }
