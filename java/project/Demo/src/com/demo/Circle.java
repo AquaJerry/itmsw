b6564d74dd617b277f7e9a2f5e791f428534b405
aquajerry <huopopo_zeyang@163.com>
2018-07-30 16:17:22 +0800

    feat: com.demo.{Shape,Circle}: inheritance

---
+++ java/project/Demo/src/com/demo/Circle.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Circle extends Shape {
+
+  public static void main(String[] args) {
+    Shape s = new Shape();
+  }
+}
---
+++ java/project/Demo/src/com/demo/Shape.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Shape {
+  public Shape() {
+    System.out.println("Parent class");
+  }
+
+  public void print() {
+    System.out.println("Shape");
+  }
+}
