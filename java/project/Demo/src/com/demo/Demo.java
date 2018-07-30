8b54019c78bfd39e9a1eeb5b74be682ce92dba35
aquajerry <huopopo_zeyang@163.com>
2018-07-30 12:09:21 +0800

    feat: com.demo.Demo: using variable

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,7 +3,8 @@ package com.demo;
 public class Demo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    int age = 30;
+    System.out.println("age=" + age);
   }
 
 }
