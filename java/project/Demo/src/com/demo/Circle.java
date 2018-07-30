34c2920d884e25323653816b79d27a2957379e71
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:16:53 +0800

    update: com.demo.Circle: rename print => print1

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -3,13 +3,13 @@ package com.demo;
 public class Circle extends Shape {
 
   @Override
-  public String print(String str) {
+  public String print1(String str) {
     System.out.println(str + " in circle");
     return "hello";
   }
 
   public static void main(String[] args) {
     Circle c = new Circle();
-    c.print("tom");
+    c.print1("tom");
   }
 }
