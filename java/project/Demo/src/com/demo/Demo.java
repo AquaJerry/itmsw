8613b08fb633a50671dadf6f7fd3ad49e17f3e5c
aquajerry <huopopo_zeyang@163.com>
2018-07-30 16:04:54 +0800

    update: com.demo.Demo: Welcome to Java lesson 1.10

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -1,17 +1,9 @@
 package com.demo;
 
 public class Demo {
-  static void sum(int a, float b) {
-    System.out.println("a=" + a);
-    System.out.println("b=" + b);
-    //return a + b;
-    System.out.println(a + "+" + b + "=" + (a+b));
-  }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    sum(5, (float)6.5d);
-    //System.out.println("s=" + s);
+
   }
 
 }
