5a5a698dc78bd7e0c5b23edeab470b61ab0f0fef
aquajerry <huopopo_zeyang@163.com>
2018-07-30 18:10:09 +0800

    update: com.demo.Circle: remove print()

--- java/project/Demo/src/com/demo/Circle.java
+++ java/project/Demo/src/com/demo/Circle.java
@@ -2,10 +2,10 @@ package com.demo;
 
 public class Circle extends Shape {
 
-  @Override
+  /*@Override
   public void print(String str) {
     System.out.println(str + " in circle");
-  }
+  }*/
 
   public static void main(String[] args) {
     Circle c = new Circle();
