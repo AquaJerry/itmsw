7b8de42a56eb0c318ab44b913cc79c48451c27c3
aquajerry <huopopo_zeyang@163.com>
2018-08-07 17:08:38 +0800

    feat: com.demo: use "private" to hide information

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,5 +1,16 @@
 package com.demo;
 
 public class A {
+  private void sayHello() {
+    System.out.println("hello");
+  }
 
+  public void print() {
+    sayHello();
+  }
+
+  public static void main(String[] args) {
+    A a = new A();
+    a.print();
+  }
 }
