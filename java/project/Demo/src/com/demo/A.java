00b87bf3c7622cfebc85d872ea64e72e1dae3640
aquajerry <huopopo_zeyang@163.com>
2018-08-07 15:32:09 +0800

    feat: com.demo.A: using this

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,6 +1,18 @@
 package com.demo;
 
 public class A {
+  public void sayHello() {
+    System.out.println("Hello");
+  }
 
+  public void print() {
+    System.out.println("print()");
+    this.sayHello();
+  }
+
+  public static void main(String[] args) {
+    A a = new A();
+    a.print();
+  }
 
 }
