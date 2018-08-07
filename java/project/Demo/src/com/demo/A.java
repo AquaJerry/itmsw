9c971b24a4f63502cdb6460db5c3cf7d07b01284
aquajerry <huopopo_zeyang@163.com>
2018-08-07 20:04:31 +0800

    update: com.demo.A to say why main() is static

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -12,7 +12,8 @@ public class A {
   }
 
   public static void main(String[] args) {
-    A a1 = new A();
+    System.out.println(A.b);
+    /*A a1 = new A();
     System.out.println(a1);
     A a2 = new A();
     System.out.println(a2);
@@ -23,6 +24,6 @@ public class A {
     System.out.println(a1.b == a2.b);
     A.sayHello();
     sayHello();
-    System.out.println(b);
+    System.out.println(b);*/
   }
 }
