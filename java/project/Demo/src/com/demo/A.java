73abe84319f5f1beaa62d92d6d02466f3297040d
aquajerry <huopopo_zeyang@163.com>
2018-08-07 16:20:26 +0800

    update: com.demo: B override A.sayHello

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -4,6 +4,9 @@ public class A {
   public void print() {
     System.out.println("print() in A");
   }
+  public void sayHello() {
+    System.out.println("sayHello() in A");
+  }
   public static void main(String[] args) {
     B b = new B();
     b.print();
@@ -16,8 +19,11 @@ class B extends A {
   @Override
   public void print() {
     // TODO Auto-generated method stub
-    super.print();
+    sayHello();
     System.out.println("print() in B");
   }
 
+  public void sayHello() {
+    System.out.println("sayHello() in B");
+  }
 }
