17bc9f394832aca12d1a114c770d6c4417174d1c
aquajerry <huopopo_zeyang@163.com>
2018-08-07 18:15:38 +0800

    update: com.demo: "final" object

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -3,6 +3,7 @@ package com.demo;
 public class A {
   private final int age = 10;
   private final String name = "tom";
+  private final Demo demo = new Demo();
   public void sayHello() {
     System.out.println("hello");
   }
@@ -10,6 +11,7 @@ public class A {
     A a = new A();
     //a.age = 10;
     //a.name = "andy";
+    a.demo = new Demo();
   }
 
 }
