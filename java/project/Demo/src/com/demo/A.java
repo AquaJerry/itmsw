9c000b6f31177f37a55aa33f5909c1bc9a1fe762
aquajerry <huopopo_zeyang@163.com>
2018-08-07 18:04:37 +0800

    update: com.demo: "final" field/variable

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,11 +1,13 @@
 package com.demo;
 
 public class A {
+  private final int age;
   public void sayHello() {
     System.out.println("hello");
   }
   public static void main(String[] args) {
-
+    A a = new A();
+    a.age = 10;
   }
 
 }
