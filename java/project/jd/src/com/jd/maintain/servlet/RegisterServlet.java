c34547969cb7cef2c779ac9a1a2a16d6b194ab39
aquajerry <huopopo_zeyang@163.com>
2018-08-07 14:19:46 +0800

    feat: com.jd.maintain.servlet.RegisterServlet: Capture form data

---
+++ java/project/jd/src/com/jd/maintain/servlet/RegisterServlet.java
@@ -0,0 +1,51 @@
+package com.jd.maintain.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/maintain/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public RegisterServlet() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.setCharacterEncoding("utf-8");
+    String userName = request.getParameter("userName");
+    String password = request.getParameter("password");
+    String phone = request.getParameter("phone");
+    String validateCode = request.getParameter("validateCode");
+    System.out.println("User Register Infomation:");
+    System.out.println("User Name: " + userName);
+    System.out.println("Pasword: " + password);
+    System.out.println("Phone: " + phone);
+    System.out.println("Validate Code: " + validateCode);
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+
+}
