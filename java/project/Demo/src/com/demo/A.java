5a36129d12de56c9ba5419d292e67df4b9d774b2
aquajerry <huopopo_zeyang@163.com>
2018-08-07 16:08:24 +0800

    update: com.demo: B override A.print

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,9 +1,23 @@
 package com.demo;
 
 public class A {
-
+  public void print() {
+    System.out.println("print() in A");
+  }
   public static void main(String[] args) {
+    B b = new B();
+    b.print();
+  }
 
+}
+
+class B extends A {
+
+  @Override
+  public void print() {
+    // TODO Auto-generated method stub
+    //super.print();
+    System.out.println("print() in B");
   }
 
 }
