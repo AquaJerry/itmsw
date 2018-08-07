1f031dd747639dd20710007af00ce40634533650
aquajerry <huopopo_zeyang@163.com>
2018-08-07 21:34:01 +0800

    update: com.demo.A: Welcome to Java lesson 2.7

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,22 +1,8 @@
 package com.demo;
 
 public class A {
-  public void test() {
-    try {
-      System.out.println(5/0);
-      System.out.println(Class.forName("com.demo.B2"));
-    } catch (ArithmeticException e) {
-      System.out.println("Divide-by-zero Error");
-    } catch (ClassNotFoundException e) {
-      System.out.println("Class B2 Not Found");
-    } catch (Exception e) {
-      System.out.println("Exception Caught");
-    } finally {
-      System.out.println("You can see me.");
-    }
-  }
+
   public static void main(String[] args) {
-    A a = new A();
-    a.test();
+
   }
 }
