6153b6d607d25cb734410950de0c4681629b490e
aquajerry <huopopo_zeyang@163.com>
2018-08-07 15:36:40 +0800

    update: com.demo.A: ignore "this"

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 public class A {
+  String userName;
+  int age;
+
   public void sayHello() {
     System.out.println("Hello");
   }
@@ -8,6 +11,8 @@ public class A {
   public void print() {
     System.out.println("print()");
     this.sayHello();
+    System.out.println("userName=" + userName);
+    System.out.println("age=" + age);
   }
 
   public static void main(String[] args) {
