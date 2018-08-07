56ab891adecfc29dc7f7a3cbdf062e4cc6488143
aquajerry <huopopo_zeyang@163.com>
2018-08-07 20:26:29 +0800

    feat: com.demo.A: using "finally"

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -3,9 +3,11 @@ package com.demo;
 public class A {
   public void test() {
     try {
-      System.out.println(5/0);
+      System.out.println(5/1);
     } catch (ArithmeticException e) {
       System.out.println("Divide-by-zero Error");
+    } finally {
+      System.out.println("You can see me.");
     }
   }
   public static void main(String[] args) {
