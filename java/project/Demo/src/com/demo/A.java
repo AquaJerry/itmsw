a7a086068c7d30b0421402598b4354f4a9a55fd7
aquajerry <huopopo_zeyang@163.com>
2018-08-07 17:04:56 +0800

    update: com.demo: Welcome to Java lesson 2.3

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,45 +1,5 @@
 package com.demo;
 
 public class A {
-  String userName;
-  public A(String userName) {
-    this.userName = userName;
-    System.out.println("userName=" + userName);
-  }
-  public A() {
 
-    System.out.println("A()");
-    // TODO Auto-generated constructor stub
-  }
-  public void print() {
-    System.out.println("print() in A");
-  }
-  public void sayHello() {
-    System.out.println("sayHello() in A");
-  }
-  public static void main(String[] args) {
-    B b = new B();
-    b.print();
-  }
-
-}
-
-class B extends A {
-
-  public B() {
-    super("tom");
-    System.out.println("B()");
-    // TODO Auto-generated constructor stub
-  }
-
-  @Override
-  public void print() {
-    // TODO Auto-generated method stub
-    super.sayHello();
-    System.out.println("print() in B");
-  }
-
-  public void sayHello() {
-    System.out.println("sayHello() in B");
-  }
 }
