f43191bdb40c11f3bfdd3f8a1e1eb5a704f8a20b
aquajerry <huopopo_zeyang@163.com>
2018-08-07 21:39:16 +0800

    feat: com.demo.A: using throw

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,8 +1,17 @@
 package com.demo;
 
 public class A {
+  public void test() {
+    try {
+      throw new Exception();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
   public static void main(String[] args) {
-
+    A a = new A();
+    a.test();
   }
 }
