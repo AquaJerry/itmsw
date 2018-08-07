67223a4d83b1035090d1150df7b0e2a6927b4cc2
aquajerry <huopopo_zeyang@163.com>
2018-08-07 18:18:14 +0800

    update: com.demo: "final" variable

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -5,10 +5,13 @@ public class A {
   private final String name = "tom";
   private final Demo demo = new Demo();
   public void sayHello() {
+    final int num = 10;
+    System.out.println("num=" + num);
     System.out.println("hello");
   }
   public static void main(String[] args) {
     A a = new A();
+    a.sayHello();
     //a.age = 10;
     //a.name = "andy";
     //a.demo = new Demo();
