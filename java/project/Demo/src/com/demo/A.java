a4c8d24656de24bc445bce241a20048dac10d371
aquajerry <huopopo_zeyang@163.com>
2018-08-07 21:10:01 +0800

    test: com.demo.A.test(): ClassNotFoundException

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -4,11 +4,11 @@ public class A {
   public void test() {
     try {
       System.out.println(5/1);
-      System.out.println(Class.forName("com.demo.B"));
+      System.out.println(Class.forName("com.demo.B2"));
     } catch (ArithmeticException e) {
       System.out.println("Divide-by-zero Error");
     } catch (ClassNotFoundException e) {
-      System.out.println("Class B Not Found");
+      System.out.println("Class B2 Not Found");
     } finally {
       System.out.println("You can see me.");
     }
