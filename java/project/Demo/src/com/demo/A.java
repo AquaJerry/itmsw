ce3d22a396d32ab1432042863772735ba07ffdf9
aquajerry <huopopo_zeyang@163.com>
2018-08-07 18:09:45 +0800

    fix: com.demo.A: error: cannot assign a value to final variable age

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,13 +1,15 @@
 package com.demo;
 
 public class A {
-  private final int age;
+  private final int age = 10;
+  private final String name = "tom";
   public void sayHello() {
     System.out.println("hello");
   }
   public static void main(String[] args) {
     A a = new A();
-    a.age = 10;
+    //a.age = 10;
+    //a.name = "andy";
   }
 
 }
