229039e6fce0cfe30b8d5b9f251c8a5f037fb991
aquajerry <huopopo_zeyang@163.com>
2018-08-07 19:20:38 +0800

    update: com.demo.A: field context

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 public class A {
+  private B b = new B();
 
   public static void main(String[] args) {
     A a1 = new A();
@@ -8,5 +9,7 @@ public class A {
     A a2 = new A();
     System.out.println(a2);
     System.out.println(a1 == a2);
+    System.out.println("a1.b=" + a1.b);
+    System.out.println("a2.b=" + a2.b);
   }
 }
