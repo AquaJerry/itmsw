8bd9c3ec3ff403a3f12fe5b1ebbd9146b29bc91f
aquajerry <huopopo_zeyang@163.com>
2018-08-07 21:22:21 +0800

    feat: com.demo.A: universal exception

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -3,11 +3,11 @@ package com.demo;
 public class A {
   public void test() {
     try {
-      System.out.println(5/0);
+      System.out.println(5/1);
       System.out.println(Class.forName("com.demo.B2"));
     } catch (ArithmeticException e) {
       System.out.println("Divide-by-zero Error");
-    } catch (ClassNotFoundException e) {
+    } catch (Exception e) {
       System.out.println("Class B2 Not Found");
     } finally {
       System.out.println("You can see me.");
