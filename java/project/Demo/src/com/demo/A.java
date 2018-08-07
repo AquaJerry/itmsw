25336f1a9fbe9cf71c8e22f4e021cdcfb52db0e7
aquajerry <huopopo_zeyang@163.com>
2018-08-07 16:04:11 +0800

    update: com.demo.A: Welcome to Java lesson 2.2

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,43 +1,9 @@
 package com.demo;
 
 public class A {
-  String userName;
-  int age;
-
-
-
-  public A() {
-    System.out.println("A()");
-  }
-
-
-  public A(String userName) {
-    System.out.println("A(userName)");
-    this.userName = userName;
-  }
-
-
-  public A(String userName, int age) {
-    this(userName);
-    this.userName = userName;
-    this.age = age;
-  }
-
-
-  public void sayHello() {
-    System.out.println("Hello");
-  }
-
-  public void print() {
-    System.out.println("print()");
-    this.sayHello();
-    System.out.println("userName=" + userName);
-    System.out.println("age=" + age);
-  }
 
   public static void main(String[] args) {
-    A a = new A("tom", 12);
-    a.print();
+
   }
 
 }
