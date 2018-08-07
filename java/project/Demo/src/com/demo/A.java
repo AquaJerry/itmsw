80aee1d9ab3a578835d5fc3f83eaa24a1c9bf6fa
aquajerry <huopopo_zeyang@163.com>
2018-08-07 19:06:08 +0800

    update: com.demo.A: Welcome to Java lesson 2.5

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,22 +1,8 @@
 package com.demo;
 
 public class A {
-  private final int age = 10;
-  private final String name = "tom";
-  private final Demo demo = new Demo();
-  public void sayHello() {
-    final int num;
-    //System.out.println("num=" + num);
-    num = 20;
-    num = 30;
-    System.out.println("hello");
-  }
-  public static void main(String[] args) {
-    A a = new A();
-    a.sayHello();
-    //a.age = 10;
-    //a.name = "andy";
-    //a.demo = new Demo();
-  }
 
+  public static void main(String[] args) {
+
+  }
 }
