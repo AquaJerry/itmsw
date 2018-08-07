fef50d373deb13d407307bd1f872c6327570b50b
aquajerry <huopopo_zeyang@163.com>
2018-08-07 21:26:24 +0800

    update: com.demo.A: append certain exception to catches

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -3,11 +3,13 @@ package com.demo;
 public class A {
   public void test() {
     try {
-      System.out.println(5/1);
+      System.out.println(5/0);
       System.out.println(Class.forName("com.demo.B2"));
     } catch (ArithmeticException e) {
       System.out.println("Divide-by-zero Error");
     } catch (Exception e) {
+      System.out.println("Exception Caught");
+    } catch (ClassNotFoundException e) {
       System.out.println("Class B2 Not Found");
     } finally {
       System.out.println("You can see me.");
