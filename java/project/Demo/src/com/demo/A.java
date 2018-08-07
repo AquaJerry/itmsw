d79688c8a6b9d3dc41313048f8ed107458608762
aquajerry <huopopo_zeyang@163.com>
2018-08-07 20:13:25 +0800

    update: com.demo.A: Add test()

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,8 +1,11 @@
 package com.demo;
 
 public class A {
-
+  public void test() {
+    System.out.println(5/0);
+  }
   public static void main(String[] args) {
-
+    A a = new A();
+    a.test();
   }
 }
