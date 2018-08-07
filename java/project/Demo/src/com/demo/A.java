49429a2ac3ab57b5479f12cbd94389ffcb04a708
aquajerry <huopopo_zeyang@163.com>
2018-08-07 20:48:50 +0800

    fix: com.demo.A: error: unreported exception ClassNotFoundException; must be caught or declared to be thrown

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -3,10 +3,12 @@ package com.demo;
 public class A {
   public void test() {
     try {
-      System.out.println(5/0);
+      System.out.println(5/1);
       System.out.println(Class.forName("com.demo.B"));
     } catch (ArithmeticException e) {
       System.out.println("Divide-by-zero Error");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Class B Not Found");
     } finally {
       System.out.println("You can see me.");
     }
