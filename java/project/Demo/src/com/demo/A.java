01fde5fab93f99f38b32d3540470dc87e8490963
aquajerry <huopopo_zeyang@163.com>
2018-08-07 17:49:55 +0800

    update: com.{demo,test}: Welcome to Java lesson 2.4

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,17 +1,9 @@
 package com.demo;
 
 public class A {
-  public void sayHello() {
-    System.out.println("hello");
-  }
-
-  public void print() {
-    this.sayHello();
-  }
 
   public static void main(String[] args) {
-    A a = new A();
-    a.print();
-    a.sayHello();
+
   }
+
 }
