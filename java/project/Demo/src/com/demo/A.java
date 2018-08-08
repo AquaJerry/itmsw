d921a2bb0125077716e8443b09c475f2be4c50db
aquajerry <huopopo_zeyang@163.com>
2018-08-08 10:46:02 +0800

    feat: com.demo.A: using throws

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,17 +1,13 @@
 package com.demo;
 
 public class A {
-  public void test() {
-    try {
-      throw new Exception();
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      //e.printStackTrace();
-      System.out.println("Oops! Something goes wrong.");
-    }
+  public void test() throws Exception {
+
+    throw new Exception();
+
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     A a = new A();
     a.test();
   }
