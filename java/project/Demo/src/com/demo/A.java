3e88bc7bf5365ca703cf4f9609e23ba0dc324531
aquajerry <huopopo_zeyang@163.com>
2018-08-08 11:01:25 +0800

    update: com.demo.A to the newest of java lesson 2.7

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -1,20 +1,20 @@
 package com.demo;
 
 public class A {
-  public void test() throws Exception {
-
-    throw new Exception();
-
+  public void test() {
+    try {
+      System.out.println(5/0);
+      System.out.println(Class.forName("com.demo.B2"));
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (ArithmeticException e) {
+      System.out.println("Divide-by-zero Error");
+    }
   }
 
   public static void main(String[] args) {
     A a = new A();
-    try {
-      a.test();
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      //e.printStackTrace();
-      System.out.println("Exception Found");
-    }
+    a.test();
   }
 }
