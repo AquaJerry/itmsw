56c5cca6c15e951eadd7ab5bf5aad6ebb8176e7b
aquajerry <huopopo_zeyang@163.com>
2018-08-09 20:25:16 +0800

    test: com.demo.Demo: multiple statements in if block

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,7 +3,7 @@ package com.demo;
 public class Demo {
 
   public static void main(String[] args) {
-    int num = 5;
+    int num = 2;
     if (num > 3) {
       System.out.println("ok");
       System.out.println("hello");
