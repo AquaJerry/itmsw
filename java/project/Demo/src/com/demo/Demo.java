12384cba8e5643e937b231c384db89fe6f969147
aquajerry <huopopo_zeyang@163.com>
2018-08-09 20:58:24 +0800

    update: com.demo.Demo: Welcome to Java lesson 2.14

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,18 +3,7 @@ package com.demo;
 public class Demo {
 
   public static void main(String[] args) {
-    int grade = 101;
-    if (grade >= 0 && grade < 60) {
-      System.out.println("grade=" + grade + "Failed");
-    } else if (grade >= 60 && grade < 70) {
-      System.out.println("grade=" + grade + "Passed");
-    } else if (grade >= 70 && grade < 90) {
-      System.out.println("grade=" + grade + "Good");
-    } else if (grade >= 90 && grade <= 100) {
-      System.out.println("grade=" + grade + "Excellent");
-    } else {
-      System.out.println("The grade is illegal.");
-    }
+
 
   }
 
