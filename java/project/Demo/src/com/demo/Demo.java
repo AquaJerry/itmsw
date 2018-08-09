d90cc61d8d86fdf16e0670126bc096b48af84d53
aquajerry <huopopo_zeyang@163.com>
2018-08-09 21:08:25 +0800

    update: com.demo.Demo: another way to calculate 1+2+3+...+100

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
   public static void main(String[] args) {
     int sum = 0;
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 100; i >= 1; i--) {
       sum = sum + i;
     }
     System.out.println("sum=" + sum);
