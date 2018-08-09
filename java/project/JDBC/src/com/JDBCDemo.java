cb482956ec49fe1b50ed536838aad8f73ef1cee2
aquajerry <huopopo_zeyang@163.com>
2018-08-09 17:33:39 +0800

    feat: com.JDBCDemo: Get database connection instance

--- java/project/JDBC/src/com/JDBCDemo.java
+++ java/project/JDBC/src/com/JDBCDemo.java
@@ -1,15 +1,27 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCDemo {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
+      String url = "jdbc:mysql://localhost:3306/jd";
+      String user = "root";
+      String password = "root";
+      Connection conn = DriverManager.getConnection(url, user, password);
+      System.out.println("conn=" + conn);
     } catch (ClassNotFoundException e) {
       // TODO Auto-generated catch block
       //e.printStackTrace();
       System.out.println("Database Driver Error");
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
   }
 
