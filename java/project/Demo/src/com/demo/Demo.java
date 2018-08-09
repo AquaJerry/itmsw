da29662ff18657e256ec25c5c6d8f0a796edccb9
aquajerry <huopopo_zeyang@163.com>
2018-08-09 21:05:47 +0800

    update: com.demo.Demo: 1+2+3+...+100=

--- java/project/Demo/src/com/demo/Demo.java
+++ java/project/Demo/src/com/demo/Demo.java
@@ -3,9 +3,11 @@ package com.demo;
 public class Demo {
 
   public static void main(String[] args) {
-    for (int i = 1; i <= 10; i++) {
-      System.out.println("i=" + i);
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      sum = sum + i;
     }
+    System.out.println("sum=" + sum);
 
   }
 
