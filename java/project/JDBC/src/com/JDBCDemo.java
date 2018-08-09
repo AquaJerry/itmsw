56e9a3ba202a5b4071f1968ed005dfdf90e1c676
aquajerry <huopopo_zeyang@163.com>
2018-08-09 16:54:21 +0800

    test: com.JDBCDemo: make exception more human

--- java/project/JDBC/src/com/JDBCDemo.java
+++ java/project/JDBC/src/com/JDBCDemo.java
@@ -5,7 +5,7 @@ public class JDBCDemo {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
+      Class.forName("com.mysql.cj.jdbc.Driver2");
     } catch (ClassNotFoundException e) {
       // TODO Auto-generated catch block
       //e.printStackTrace();
