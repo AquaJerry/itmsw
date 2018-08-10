f103aaa37b4f98b8f72e2b376201e38cbcbf624d
aquajerry <huopopo_zeyang@163.com>
2018-08-10 14:15:05 +0800

    feat: com.JDBCDemo: query user from database

--- java/project/JDBC/src/com/JDBCDemo.java
+++ java/project/JDBC/src/com/JDBCDemo.java
@@ -2,8 +2,9 @@ package com;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JDBCDemo {
   public Connection getConnection() {
@@ -16,7 +17,7 @@ public class JDBCDemo {
       conn = DriverManager.getConnection(url, user, password);
     } catch (ClassNotFoundException e) {
       // TODO Auto-generated catch block
-      //e.printStackTrace();
+      // e.printStackTrace();
       System.out.println("Database Driver Error");
     } catch (SQLException e) {
       // TODO Auto-generated catch block
@@ -58,10 +59,59 @@ public class JDBCDemo {
     }
   }
 
+  public void queryUser() {
+    Connection conn = getConnection();
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    try {
+      String sql = "select * from user_info";
+      ps = conn.prepareStatement(sql);
+      rs = ps.executeQuery();
+      System.out.println("id\tuser_name\tpassword\tphone");
+      while (rs.next()) {
+        System.out.println(
+            rs.getInt("id")
+                + "\t"
+                + rs.getString("user_name")
+                + "\t"
+                + rs.getString("password")
+                + "\t"
+                + rs.getString("phone"));
+      }
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      if (rs != null) {
+        try {
+          rs.close();
+        } catch (SQLException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+      if (ps != null) {
+        try {
+          ps.close();
+        } catch (SQLException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+      if (conn != null) {
+        try {
+          conn.close();
+        } catch (SQLException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     JDBCDemo jdbc = new JDBCDemo();
-    jdbc.saveUser();
+    jdbc.queryUser();
   }
-
 }
