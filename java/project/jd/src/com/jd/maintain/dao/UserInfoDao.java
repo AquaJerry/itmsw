d3e454938648d519b6ebd26505a150c76ed518bd
aquajerry <huopopo_zeyang@163.com>
2018-08-10 17:45:22 +0800

    feat: Add com.jd.maintain.dao.UserInfoDao.saveUserInfo

--- java/project/jd/src/com/jd/maintain/dao/UserInfoDao.java
+++ java/project/jd/src/com/jd/maintain/dao/UserInfoDao.java
@@ -29,6 +29,24 @@ public class UserInfoDao {
     return flag;
   }
 
+  public void saveUserInfo() {
+    Connection conn = JDBCUtil.getConnection();
+    PreparedStatement ps = null;
+    try {
+      String sql = "insert user_info(user_name,password,phone)value(?,?,?)";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, "用户999");
+      ps.setString(2, "333***");
+      ps.setString(3, "13712345678");
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      JDBCUtil.clear(conn, ps, null);
+    }
+  }
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     UserInfoDao userInfoDao = new UserInfoDao();
