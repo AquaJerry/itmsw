0b04a87bc31d9aec71e5725580508dfe8520ee12
aquajerry <huopopo_zeyang@163.com>
2018-08-10 17:07:30 +0800

    feat: Add com.jd.maintain.dao.UserInfoDao

---
+++ java/project/jd/src/com/jd/maintain/dao/UserInfoDao.java
@@ -0,0 +1,36 @@
+package com.jd.maintain.dao;
+
+import com.jd.common.JDBCUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserInfoDao {
+  public boolean queryByUserName(String userName) {
+    Connection conn = JDBCUtil.getConnection();
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    boolean flag = false;
+    try {
+      String sql = "select*from user_info where user_name=?";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, userName);
+      rs = ps.executeQuery();
+      if (rs.next()) {
+        flag = true;
+      }
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      JDBCUtil.clear(conn, ps, rs);
+    }
+    return flag;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+}
