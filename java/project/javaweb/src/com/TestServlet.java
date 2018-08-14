9687b8c85639eeabc99a316b05686bdea93fa9c5
aquajerry <huopopo_zeyang@163.com>
2018-08-14 15:40:16 +0800

    update: com.TestServlet: remove web address

--- java/project/javaweb/src/com/TestServlet.java
+++ java/project/javaweb/src/com/TestServlet.java
@@ -2,13 +2,11 @@ package com;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet implementation class TestServlet */
-@WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
