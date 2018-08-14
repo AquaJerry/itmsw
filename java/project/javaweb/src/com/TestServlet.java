903b7c060e8f9285c544205a857c0d67499f3e02
aquajerry <huopopo_zeyang@163.com>
2018-08-14 17:31:29 +0800

    feat: com.TestServlet: Add init()

--- java/project/javaweb/src/com/TestServlet.java
+++ java/project/javaweb/src/com/TestServlet.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
+  @Override
+  public void init() throws ServletException {
+    // TODO Auto-generated method stub
+    System.out.println("initialize");
+  }
+
   /** @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
