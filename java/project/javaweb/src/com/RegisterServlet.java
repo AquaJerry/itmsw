03dd9b0e8a1703470f779c35bf283d60e37f8bff
aquajerry <huopopo_zeyang@163.com>
2018-08-14 18:15:16 +0800

    update: javaweb.war: Welcome to Java lesson 3.4

--- java/project/javaweb/src/com/RegisterServlet.java
+++ java/project/javaweb/src/com/RegisterServlet.java
@@ -10,32 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-  @Override
-  public void init() throws ServletException {
-    // TODO Auto-generated method stub
-    System.out.println("initialize");
-  }
-
-  @Override
-  public void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    System.out.println("process request");
-    super.service(request, response);
-  }
-
-  @Override
-  public void destroy() {
-    // TODO Auto-generated method stub
-    super.destroy();
-    System.out.println("destroy");
-  }
-
   /** @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     // TODO Auto-generated method stub
-    System.out.println("test servlet");
   }
 
   /** @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) */
