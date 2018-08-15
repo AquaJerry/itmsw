308b1351fd468e4a0f2d82d506d1929dfad309ac
aquajerry <huopopo_zeyang@163.com>
2018-08-15 14:33:00 +0800

    test: com.jd.maintain.dao.UserInfoDao.queryUserInfo(userName, password) #2

--- java/project/jd/src/com/jd/maintain/dao/UserInfoDao.java
+++ java/project/jd/src/com/jd/maintain/dao/UserInfoDao.java
@@ -73,7 +73,7 @@ public class UserInfoDao {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     UserInfoDao userInfoDao = new UserInfoDao();
-    boolean flag = userInfoDao.queryUserInfo("user01", "123aaa");
+    boolean flag = userInfoDao.queryUserInfo("user01", "123aaax");
     System.out.println("flag=" + flag);
   }
 }
