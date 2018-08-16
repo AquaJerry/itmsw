2150a159e556daf33ed1018f6e2c9ddcef51e57f
aquajerry <huopopo_zeyang@163.com>
2018-08-16 16:02:15 +0800

    doc: com.{Student,demo,test}

--- java/project/Demo/src/com/demo/Shape.java
+++ java/project/Demo/src/com/demo/Shape.java
@@ -1,6 +1,11 @@
+/* Copyright (c) 2018, https://github.com/AquaJerry/itmsw. ISC License. */
+
 package com.demo;
 
+/** This is just a sample. */
 public class Shape {
+
+  /** Print a string. */
   public void print() {
     System.out.println("shape");
   }
--- java/project/Demo/src/com/test/Test.java
+++ java/project/Demo/src/com/test/Test.java
@@ -1,10 +1,14 @@
+/* Copyright (c) 2018, https://github.com/AquaJerry/itmsw. ISC License. */
+
 package com.test;
 
 import com.demo.Shape;
 import com.demo.Student;
 
+/** Test {@link Shape} and {@link Student}. */
 public class Test {
 
+  /** Fire the test. */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     Student s = new Student();
