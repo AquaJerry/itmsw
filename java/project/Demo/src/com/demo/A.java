c467b25eedc0808027a7441a964305a91e9437dd
aquajerry <huopopo_zeyang@163.com>
2018-08-17 19:21:14 +0800

    update: com.demo.[ABC]: All abstract methods must be implemented

--- java/project/Demo/src/com/demo/A.java
+++ java/project/Demo/src/com/demo/A.java
@@ -8,10 +8,16 @@ public abstract class A {
   /** Print something. */
   public abstract void print();
 
+  /** Print something. */
+  public abstract void sayHello();
+
   /** Try to construct an object of type A. */
   public static void main(String[] args) {
     // A a = new A();
-    B b = new B();
-    b.print();
+    // B b = new B();
+    // b.print();
+    C c = new C();
+    c.print();
+    c.sayHello();
   }
 }
