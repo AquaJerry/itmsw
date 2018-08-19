8c603029fb93201c47c55736e1f12d2dee293d77
aquajerry <huopopo_zeyang@163.com>
2018-08-19 14:04:40 +0800

    feat: com.TestServlet: get real path of server

---
+++ java/project/javaweb/src/com/TestServlet.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2018, https://github.com/AquaJerry/itmsw. ISC License. */
+
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** This is just a sample. */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+
+  /**
+   * Get real path.
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    String str = request.getRealPath("/");
+    System.out.println("str=" + str);
+  }
+
+  /**
+   * Do as the same as {@link #doGet doGet}.
+   *
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+}
