ff737d48b1eeb9d9bdcf8e110a607d64f837af81
aquajerry <huopopo_zeyang@163.com>
2018-08-28 17:52:15 +0800

    feat: com.jd.maintain.servlet.ValidationCodeServlet: generate 4-bit validation code

--- java/project/jd/src/com/jd/maintain/servlet/ValidationCodeServlet.java
+++ java/project/jd/src/com/jd/maintain/servlet/ValidationCodeServlet.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -17,6 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet implementation for validation code on Jingdong/Joybuy register page. */
 @WebServlet("/maintain/ValidationCodeServlet")
 public class ValidationCodeServlet extends HttpServlet {
+
+  /* The charset for validation code. */
+  private static final String CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private static final int CHARSET_SIZE = CHARSET.length();
+
+  /* The random number generator. */
+  private static final Random RANDOM = new Random();
+
+  /**
+   * Create a 4-bit random validation code.
+   *
+   * @return the validation code
+   */
+  public static String createValidationCode() {
+    String validationCode = "";
+    int index;
+    for (int i = 0; i < 4; ++i) {
+      index = RANDOM.nextInt(CHARSET_SIZE);
+      validationCode += CHARSET.charAt(index);
+    }
+    return validationCode;
+  }
 
   /**
    * Send a background image of validation code to the register page.
@@ -33,6 +56,7 @@ public class ValidationCodeServlet extends HttpServlet {
     OutputStream out = response.getOutputStream();
     ImageIO.write(image, "png", out);
     out.close();
+    System.out.println(createValidationCode());
   }
 
   /**
