11bf3f6755eb35c53fd4e55a1a83c264f4239a09
aquajerry <huopopo_zeyang@163.com>
2018-08-29 13:31:30 +0800

    update: com.jd.maintain.servlet.ValidationCodeServlet: the font style of validation code

--- java/project/jd/src/com/jd/maintain/servlet/ValidationCodeServlet.java
+++ java/project/jd/src/com/jd/maintain/servlet/ValidationCodeServlet.java
@@ -51,7 +51,7 @@ public class ValidationCodeServlet extends HttpServlet {
    */
   public static void drawValidationCode(BufferedImage image, String validationCode) {
     Graphics graphics = image.getGraphics();
-    graphics.setFont(new Font("MicrosoftYahei", Font.BOLD, /*size*/ 20));
+    graphics.setFont(new Font("MicrosoftYahei", Font.PLAIN, /*size*/ 20));
     graphics.drawString(validationCode, /*x*/ 10, /*y*/ 20);
   }
 
