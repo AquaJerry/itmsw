bbcc05a8fecad80151e3560c5e78ad6fc87a1d73
aquajerry <huopopo_zeyang@163.com>
2018-08-30 19:21:11 +0800

    fix: com.F1: blank web page

--- java/project/javaweb/src/com/F1.java
+++ java/project/javaweb/src/com/F1.java
@@ -2,9 +2,11 @@
 
 package com;
 
+import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
@@ -19,11 +21,12 @@ public class F1 implements Filter {
   }
 
   /** @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain) */
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
     System.out.println("Enter Filter 1");
 
     // pass the request along the filter chain
-    // chain.doFilter(request, response);
+    chain.doFilter(request, response);
   }
 
   /** @see Filter@init(FilterConfig) */
