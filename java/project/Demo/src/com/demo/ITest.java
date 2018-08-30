e669728f6d268187d58e1f12c6f4284c1a8e8b97
aquajerry <huopopo_zeyang@163.com>
2018-08-30 15:37:50 +0800

    update: add com.demo.ITest

---
+++ java/project/Demo/src/com/demo/ITest.java
@@ -0,0 +1,8 @@
+/* Copyright (c) 2018, https://github.com/AquaJerry/itmsw. ISC License. */
+
+package com.demo;
+
+/** This is just a sample. */
+public interface ITest {
+  public void printName();
+}
